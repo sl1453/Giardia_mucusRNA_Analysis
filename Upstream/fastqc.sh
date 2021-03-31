15c0cdfae1e429a23b9cf59f4a01f53c8145cdde
srmarzec <srmarzec@users.noreply.github.com>
2021-03-31 09:25:36 -0400

    Create fastqc.sh

---
+++ Upstream/fastqc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=fastqc --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=sm3679@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=12:00:00
+#SBATCH --mem=1G
+
+#-----------------------------------------------------------------------------#
+# This script gives quality control of fastq files #
+#-----------------------------------------------------------------------------#
+
+
+#- RUN fastqc ----------------------------------------------------------------#
+
+/home/sm3679/bin/FastQC/fastqc -o /home/sm3679/culex_biting/fastqc_dir /home/sm3679/culex_biting/trim_dir/*PE.fastq.gz
+
+#- FIN -----------------------------------------------------------------------#
