282f21099c14876e7672bf915a481123964e06c0
srmarzec <srmarzec@users.noreply.github.com>
2021-04-02 09:42:17 -0400

    Create STAR_index.sh

---
+++ Upstream/STAR_index.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=STAR_index --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=sm3679@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=12:00:00
+#SBATCH --mem=100G
+
+#-----------------------------------------------------------------------------#
+# This script makes index of the ref genome using STAR #
+#-----------------------------------------------------------------------------#
+
+module load star/2.7.1a
+
+#- Set variables ----------------------------------------------------------------#
+
+refgen_dir=/home/sm3679/culex_biting/culex_genome
+refgen_index=/home/sm3679/culex_biting/culex_genome/index_genome
+
+
+#- RUN STAR----------------------------------------------------------------#
+
+STAR --runMode genomeGenerate \
+        --genomeDir ${refgen_index} \
+        --genomeFastaFiles ${refgen_dir}/GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.fna \
+        --sjdbGTFfile ${refgen_dir}/GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.gtf
+
+#- FIN -----------------------------------------------------------------------#
