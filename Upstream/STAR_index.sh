e710df59d0fa23412de3e6f1572ef622c61aa93a
srmarzec <srmarzec@users.noreply.github.com>
2021-04-08 11:54:03 -0400

    Update STAR_index.sh

--- Upstream/STAR_index.sh
+++ Upstream/STAR_index.sh
@@ -21,6 +21,8 @@ refgen_index=/home/sm3679/culex_biting/culex_genome/index_genome
 STAR --runMode genomeGenerate \
         --genomeDir ${refgen_index} \
         --genomeFastaFiles ${refgen_dir}/GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.fna \
-        --sjdbGTFfile ${refgen_dir}/GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.gtf
+        --sjdbGTFfile ${refgen_dir}/GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.gtf \
+        --sjdbOverhang 135 \
+        --genomeSAindexNbases 13
 
 #- FIN -----------------------------------------------------------------------#
